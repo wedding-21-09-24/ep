62694e2b8b972b17c7b19018ad037afbadb224be
Pavel Samodurov <ps07t447@gmail.com>
2024-04-28 09:40:42 +0300

    add sending to google forms

--- src/components/GuestForm.js
+++ src/components/GuestForm.js
@@ -1,69 +1,189 @@
 import {Box, Button, Checkbox, FormControl, FormLabel, Input, Radio, RadioGroup, Stack, Typography} from "@mui/joy";
 import * as React from "react";
 import {FormGroup} from "@mui/material";
+import {useState} from "react";
 
 function GuestForm() {
+    const [formIsHidden, setFormIsHidden] = useState(false);
+    const [fullName, setFullName] = useState('');
+    const [presence, setPresence] = useState('');
+    const [allergy, setAllergy] = useState('');
+    const [whiteWine, setWhiteWine] = useState(false);
+    const [redWine, setRedWine] = useState(false);
+    const [champagne, setChampagne] = useState(false);
+    const [vodka, setVodka] = useState(false);
+    const [whiskey, setWhiskey] = useState(false);
+    const [cognac, setCognac] = useState(false);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('FullName:', fullName,
+            'Presence: ', presence,
+            'Allergy: ', allergy,
+            'WhiteWine: ', whiteWine,
+            'RedWine: ', redWine,
+            'Champagne: ', champagne,
+            'Vodka: ', vodka,
+            'Whiskey: ', whiskey,
+            'Cognac: ', cognac
+        );
+        setFullName('');
+        setPresence('');
+        setAllergy('');
+        setWhiteWine(false);
+        setRedWine(false);
+        setChampagne(false);
+        setVodka(false);
+        setWhiskey(false);
+        setCognac(false);
+        setFormIsHidden(true)
+
+        const googleFormUrl = prepareGoogleFormUrl();
+        fetch(googleFormUrl, {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+        });
+    }
+
+    function prepareGoogleFormUrl() {
+        const fullNameFormatted = fullName.replaceAll(" ", "+");
+        const allergyFormatted = allergy.replaceAll(" ", "+");
+        const drinks = appendDrinkChoose()
+        return `https://docs.google.com/forms/d/e/1FAIpQLSdlKhrxSktxrxrvmslmP8MeeXEXkPtbAgTPM9Fdh8RzHAvpIQ/formResponse?&submit=Submit?usp=pp_url&entry.1453880769=${fullNameFormatted}&entry.1908531472=${presence}&entry.470484814=${allergyFormatted}${drinks}`
+    }
+
+    function appendDrinkChoose() {
+        const drinksAnswer = new Map()
+        drinksAnswer.set("Белое+вино", whiteWine);
+        drinksAnswer.set("Красное+вино", redWine);
+        drinksAnswer.set("Шампанское", champagne);
+        drinksAnswer.set("Водка", vodka);
+        drinksAnswer.set("Виски", whiskey);
+        drinksAnswer.set("Коньяк", cognac);
+
+        let result = "";
+        drinksAnswer.forEach((answer, drink) => {
+            if (answer) {
+                result = result + `&entry.1322127710=${drink}`
+            }
+        })
+
+        return result;
+    }
+
+    const repeatVote = () => {
+        setFormIsHidden(false);
+    };
+
     return (
-        <Box sx={{
-            m: '1em',
-            mt: '3em'
-        }}>
-            <Typography
-                sx={{
-                    fontSize: '1.5em',
-                    mb: '0.25em'
+        <>
+            <form onSubmit={handleSubmit} hidden={formIsHidden}>
+                <Box sx={{
+                    m: '1em',
+                    mt: '3em'
                 }}>
-                Анкета гостя
-            </Typography>
-            <Typography
-                sx={{
-                    mb: '1em'
-                }}>
-                Ваши ответы на вопросы очень помогут нам при организации свадьбы
-            </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: '1.5em',
+                            mb: '0.25em'
+                        }}>
+                        Анкета гостя
+                    </Typography>
+                    <Typography
+                        sx={{
+                            mb: '1em'
+                        }}>
+                        Ваши ответы на вопросы очень помогут нам при организации свадьбы
+                    </Typography>
 
-            <Box display="flex"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <FormGroup sx={{gap: 3}}>
-                    <FormGroup>
-                        <FormLabel sx={{mb: 1}}>Имя Фамилия</FormLabel>
-                        <Input size="lg"/>
-                    </FormGroup>
-                    <FormGroup>
-                        <FormLabel sx={{mb: 1}}>Присутствие</FormLabel>
-                        <FormControl required>
-                            <RadioGroup name="presence" size="lg">
-                                <Radio value="yes" label="Я приду"/>
-                                <Radio value="maybe" label="Скажу ответ позже"/>
-                                <Radio value="no" label="Прийти не получится"/>
-                            </RadioGroup>
-                        </FormControl>
-                    </FormGroup>
-                    <FormGroup>
-                        <FormLabel sx={{mb: 1}}>Есть ли аллергия, диета или особое требование к меню?</FormLabel>
-                        <Input size="lg"/>
-                    </FormGroup>
-                    <FormGroup>
-                        <FormLabel sx={{mb: 1}}>Предпочтения по напиткам</FormLabel>
-                        <Stack
-                            sx={{gap: 2}}>
-                            <Checkbox label="Вино белое" size="lg"/>
-                            <Checkbox label="Вино красное" size="lg"/>
-                            <Checkbox label="Шампанское" size="lg"/>
-                            <Checkbox label="Виски" size="lg"/>
-                            <Checkbox label="Водка" size="lg"/>
-                            <Checkbox label="Коньяк" size="lg"/>
-                        </Stack>
-                    </FormGroup>
+                    <Box display="flex"
+                         justifyContent="center"
+                         alignItems="center"
+                    >
+                        <FormGroup sx={{gap: 3}}>
+                            <FormGroup>
+                                <FormLabel sx={{mb: 1}}>Имя Фамилия</FormLabel>
+                                <Input value={fullName}
+                                       onInput={e => setFullName(e.target.value)}
+                                       size="lg"/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel sx={{mb: 1}}>Присутствие</FormLabel>
+                                <FormControl>
+                                    <RadioGroup
+                                        value={presence}
+                                        onChange={e => setPresence(e.target.value)}
+                                        size="lg">
+                                        <Radio value="Да" label="Я приду"/>
+                                        <Radio value="Скажу+ответ+позже" label="Скажу ответ позже"/>
+                                        <Radio value="Прийти+не+получится" label="Прийти не получится"/>
+                                    </RadioGroup>
+                                </FormControl>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel sx={{mb: 1}}>Есть ли аллергия, диета или особое требование к
+                                    меню?</FormLabel>
+                                <Input value={allergy}
+                                       onInput={e => setAllergy(e.target.value)}
+                                       size="lg"/>
+                            </FormGroup>
+                            <FormGroup>
+                                <FormLabel sx={{mb: 1}}>Предпочтения по напиткам</FormLabel>
+                                <Stack
+                                    sx={{gap: 2}}>
+                                    <Checkbox label="Вино белое"
+                                              checked={whiteWine}
+                                              onChange={e => setWhiteWine(e.target.checked)}
+                                              size="lg"/>
+                                    <Checkbox label="Вино красное"
+                                              checked={redWine}
+                                              onChange={e => setRedWine(e.target.checked)}
+                                              size="lg"/>
+                                    <Checkbox label="Шампанское"
+                                              checked={champagne}
+                                              onChange={e => setChampagne(e.target.checked)}
+                                              size="lg"/>
+                                    <Checkbox label="Виски"
+                                              checked={whiskey}
+                                              onChange={e => setWhiskey(e.target.checked)}
+                                              size="lg"/>
+                                    <Checkbox label="Водка"
+                                              checked={vodka}
+                                              onChange={e => setVodka(e.target.checked)}
+                                              size="lg"/>
+                                    <Checkbox label="Коньяк"
+                                              checked={cognac}
+                                              onChange={e => setCognac(e.target.checked)}
+                                              size="lg"/>
+                                </Stack>
+                            </FormGroup>
 
-                    <Button
-                        size="lg" color="neutral"
-                        type="submit">Отправить</Button>
-                </FormGroup>
+                            <Button
+                                size="lg" color="neutral"
+                                type="submit">Отправить</Button>
+                        </FormGroup>
+                    </Box>
+                </Box>
+            </form>
+            <Box hidden={!formIsHidden} sx={{
+                m: '1em',
+                mt: '3em'
+            }}>
+                <Typography
+                    sx={{
+                        fontSize: '1.5em',
+                        mb: '0.25em'
+                    }}>
+                    Спасибо за ответ!
+                </Typography>
+                <Button
+                    onClick={repeatVote}
+                    size="lg" color="neutral">Заполнить анкету еще раз</Button>
             </Box>
-        </Box>
+        </>
     );
 }
 
